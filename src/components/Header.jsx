3bba0219305774e6d177cf81c1ac53e908660934
Edilberto Vazquez Luna <edi_vazquez12@hotmail.com>
2021-05-20 02:42:17 -0500

    page finished

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,83 @@
 import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import classNames from "classnames";
+import PropTypes from "prop-types";
+import gravatar from "../utils/gravatar";
+import { logoutRequest } from "../actions/index";
 import "../assets/styles/components/Header.scss";
 import logo from "../assets/static/logo-platzi-video-BW2.png";
 import userIcon from "../assets/static/user-icon.png";
 
-const Header = () => {
+const Header = (props) => {
+  const { user, isLogin, isRegister } = props;
+  const hasUser = Object.keys(user).length > 0;
+
+  const handleLogout = () => {
+    props.logoutRequest({});
+  };
+
+  const headerClass = classNames("header", {
+    isLogin,
+    isRegister,
+  });
+
   const profile = "Perfil";
-  const account = "Cuenta";
-  const signOff = "Cerrar Sesión";
+  const signOff = "Cerra Sesión";
+  const login = "Iniciar Sesión";
   return (
-    <header className="header">
-      <img className="header__img" src={logo} alt="Platzi Video" />
+    <header className={headerClass}>
+      <Link to="/">
+        <img className="header__img" src={logo} alt="Platzi Video" />
+      </Link>
       <div className="header__menu">
         <div className="header__menu--profile">
-          <img src={userIcon} alt="" />
+          <img
+            src={Object.keys(user).length > 0 ? gravatar(user.email) : userIcon}
+            alt={user.email}
+          />
           <p>{profile}</p>
         </div>
         <ul>
-          <li>
-            <a href="/">{account}</a>
-          </li>
-          <li>
-            <a href="/">{signOff}</a>
-          </li>
+          {/* {hasUser ? (
+            <li>
+              <Link to="/cuenta">{user.name}</Link>
+            </li>
+          ) : null} */}
+          {hasUser ? (
+            <>
+              <li>
+                <Link to="/cuenta">{user.name}</Link>
+              </li>
+              <li>
+                <Link to="#logout" onClick={handleLogout}>
+                  {signOff}
+                </Link>
+              </li>
+            </>
+          ) : (
+            <li>
+              <Link to="/login">{login}</Link>
+            </li>
+          )}
         </ul>
       </div>
     </header>
   );
 };
 
-export default Header;
+Header.propTypes = {
+  user: PropTypes.object,
+};
+
+const mapStateToProps = (state) => {
+  return {
+    user: state.user,
+  };
+};
+
+const mapDispatchToProps = {
+  logoutRequest,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,12 +1,16 @@
 import React from "react";
+import classNames from "classnames";
 import "../assets/styles/components/Search.scss";
 
-const Search = () => {
+const Search = ({ isHome }) => {
   const mainTitle = "¿Qué quieres ver hoy?";
+  const inputStyle = classNames("input", {
+    isHome,
+  });
   return (
     <section className="main">
       <h2 className="main__title">{mainTitle}</h2>
-      <input type="text" className="input" placeholder="Buscar..." />
+      <input type="text" className={inputStyle} placeholder="Buscar..." />
     </section>
   );
 };
