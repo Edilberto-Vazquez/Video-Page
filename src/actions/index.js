f7a1034aa922ba302b550264c45961925245b02c
Edilberto Vazquez Luna <edi_vazquez12@hotmail.com>
2021-05-20 02:43:04 -0500

    page finished

---
+++ src/actions/index.js
@@ -0,0 +1,31 @@
+import { actions } from "../actions/actions";
+
+export const setFavorite = (payload) => ({
+  type: actions.setFavorite,
+  payload,
+});
+
+export const deleteFavorite = (payload) => ({
+  type: actions.deleteFavorite,
+  payload,
+});
+
+export const loginRequest = (payload) => ({
+  type: actions.loginRequest,
+  payload,
+});
+
+export const logoutRequest = (payload) => ({
+  type: actions.logoutRequest,
+  payload,
+});
+
+export const registerRequest = (payload) => ({
+  type: actions.registerRequest,
+  payload,
+});
+
+export const getVideoSource = (payload) => ({
+  type: actions.getVideoSource,
+  payload,
+});
---
+++ src/containers/Register.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { registerRequest } from "../actions/index";
+import { Link } from "react-router-dom";
+import "../assets/styles/components/Register.scss";
+import Header from "../components/Header";
+
+const Register = (props) => {
+  const [form, setForm] = useState({ email: "", password: "", name: "" });
+
+  const handleInput = (event) => {
+    setForm({ ...form, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    props.registerRequest(form);
+    props.history.push("/");
+  };
+
+  return (
+    <>
+      <Header isRegister />
+      <section className="register">
+        <section className="register__container">
+          <h2>Regístrate</h2>
+          <form className="register__container--form" onSubmit={handleSubmit}>
+            <input
+              className="input"
+              type="text"
+              placeholder="Nombre"
+              name="name"
+              onChange={handleInput}
+            />
+            <input
+              className="input"
+              type="text"
+              placeholder="Correo"
+              name="email"
+              onChange={handleInput}
+            />
+            <input
+              className="input"
+              type="password"
+              placeholder="Contraseña"
+              name="password"
+              onChange={handleInput}
+            />
+            <button className="button">Registrarme</button>
+          </form>
+          <Link to="/login">Iniciar sesón</Link>
+        </section>
+      </section>
+    </>
+  );
+};
+
+const mapDispatchToProps = {
+  registerRequest,
+};
+
+export default connect(null, mapDispatchToProps)(Register);
